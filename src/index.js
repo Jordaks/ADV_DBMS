e9ca7d34118190a0742ae9ff8c69b39d684d60e3
Dannn <Jordaks>
2025-03-30 18:08:58 +0800

    cart/footer

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav a');
+window.addEventListener('scroll', () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+});
